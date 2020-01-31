ce3eeafd83645ad077da26a0d8412ed81a94b8d7
Harley McDowell <hmcdowell95@gmail.com>
2020-01-31 01:23:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 def prime?(x)
-  if x == 2 || x == 3 || x % 2 != 0 || x % 3 != 0
-    true
-  else
-    false
+  if x < 2 
+    false 
+  else 
+    (2..x).to_all? do |num|
+      
+    
+    
   end
 end

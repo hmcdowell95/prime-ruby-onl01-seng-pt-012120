fb32ac1137474e87b0588aeae33cb58736b69e88
Harley McDowell <hmcdowell95@gmail.com>
2020-01-31 00:59:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,4 @@
 def prime?(x)
-  if x <= 0 
-    false
-  elsif if x == 2 || x == -2 || x % 2 != 0 || x % 3 != 0
-    true 
-  elsif x % -2 != 0 || x % -3 != 0
-    true 
-  else
-    false 
+  if x 
   end
 end

7566aef2bcb97ed85903e4bc8a427bc55daf89ec
Harley McDowell <hmcdowell95@gmail.com>
2020-01-31 00:31:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  na = []
+  num.collect do |x|
+    if x == 2 || x == -2 || x % 2 != 0 || x % 3 != 0
+      na << x 
+    else
+      false
+    end
+  end
+  na
+end
